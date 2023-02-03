01946e081494427b5066e35066818ff6897c2a62
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-03 17:00:43 -0600

    build course and courseSchema

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground') // in a real app, this parameter should come from a configuration file
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.log('Error: ', error.message));
+
+const course = {
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+};
+
+const courseSchema = new mongoose.Schema(course);

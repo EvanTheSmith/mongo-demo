5f83ba4a496a1b82e2c1e2022ea6ef0431e5d112
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-05 23:48:08 -0600

    refactored courseSchema; built model for courses

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ mongoose.connect('mongodb://localhost/playground') // in a real app, this parame
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.log('Error: ', error.message));
 
-const course = {
+const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [ String ],
     date: { type: Date, default: Date.now },
     isPublished: Boolean
-};
+});
 
-const courseSchema = new mongoose.Schema(course);
+const Course = mongoose.model('Course', courseSchema); // Paschal case (capitalized first letter)
+const course = new Course({ // Camel Case (lowercase first letter)
+    name: 'Node.js Course',
+    author: 'Mosh',
+    tags: ['node', 'backend'] // this complex value is only possible in a document/noSQL database like MongoDB
+});

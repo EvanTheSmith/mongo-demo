a22022ce3b5cb6dacec482811e3dba415212890e
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-06 02:29:36 -0600

    improved comments in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true }) // in a real app, this parameter should come from a configuration file
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.log('Error: ', error.message));
+.then(() => console.log('Connected to MongoDB')) // log to console when successful
+.catch((error) => console.log('Error: ', error.message)); // or log error if not
 
 const courseSchema = new mongoose.Schema({
     name: String,
@@ -15,9 +15,9 @@ const Course = mongoose.model('Course', courseSchema); // creates a class from t
 
 async function createCourse() {
 const course = new Course({ // creates an individual course from the class
-    name: 'Node.js Course',
+    name: 'Angular Course',
     author: 'Mosh',
-    tags: ['node', 'backend'], // this complex value is only possible in a document/noSQL database like MongoDB
+    tags: ['angular', 'frontend'], // this complex value is only possible in a document/noSQL database like MongoDB
     isPublished: true
 });
 const result = await course.save(); // save to database asynchronously; use await to wait for result

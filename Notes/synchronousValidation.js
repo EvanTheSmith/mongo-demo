6b751ad4aa9df6d54d073d0265211d802bb05da2
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-11 17:29:45 -0600

    testing asynchronous/synchronous validators

---
+++ Notes/synchronousValidation.js
@@ -0,0 +1,30 @@
+// SYNCHRONOUS
+const courseSchema4 = new mongoose.Schema({
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(value) {
+                return value && value.length>0;
+            },
+            message: 'a course must have at least one tag'
+        }
+    }
+});
+
+// ASYNCHRONOUS
+const courseSchema4 = new mongoose.Schema({
+    tags: {
+        type: Array,
+        validate: {
+            isAsync: true,
+            validator: function(value, callback) {
+                setTimeout(() => {
+                    // Async Code Goes Here
+                    const result = value && value.length>0;
+                    callback(result);
+                }, 1000);
+            },
+            message: 'a course must have at least one tag'
+        }
+    }
+});
